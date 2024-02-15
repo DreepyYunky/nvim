d95ee97200bfb71c1d8f0bee12a7aa4a7edcee1c
Yunky <nwokeechika@gmail.com>
2024-02-15 20:14:44 +0100

    Various Updates

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ return {
 		"williamboman/mason.nvim",
 		lazy = false,
 		config = function()
-			require("mason").setup({ PATH = "prepend" })
+			require("mason").setup()
 		end,
 	},
 	{
@@ -39,10 +39,43 @@ return {
 
 			lsp.gdscript.setup {
 				force_setup = true,
-				cmd = {'ncat', '127.0.0.1', '6008'}, -- the important trick for Windows!
+				cmd = { 'ncat', '127.0.0.1', '6008' }, -- the important trick for Windows!
 				root_dir = require('lspconfig.util').root_pattern('project.godot', '.git'),
-    				filetypes = {'gd', 'gdscript', 'gdscript3' }
+				filetypes = { 'gd', 'gdscript', 'gdscript3' }
 			}
+			lsp.ols.setup {}
+			local M = {}
+			M.icons = {
+				Class = " ",
+				Color = " ",
+				Constant = " ",
+				Constructor = " ",
+				Enum = " ",
+				EnumMember = " ",
+				Field = "󰄶 ",
+				File = " ",
+				Folder = " ",
+				Function = " ",
+				Interface = "󰜰",
+				Keyword = "󰌆 ",
+				Method = "ƒ ",
+				Module = "󰏗 ",
+				Property = " ",
+				Snippet = "󰘍 ",
+				Struct = " ",
+				Text = " ",
+				Unit = " ",
+				Value = "󰎠 ",
+				Variable = " ",
+			}
+			function M.setup()
+				local kinds = vim.lsp.protocol.CompletionItemKind
+				for i, kind in ipairs(kinds) do
+					kinds[i] = M.icons[kind] or kind
+				end
+			end
+
+			return M
 		end
 	},
 }
