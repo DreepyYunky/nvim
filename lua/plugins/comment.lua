01bfe678ed9bb6bcce738dcc718ac6a62633ebbe
Yunky <nwokeechika@gmail.com>
2024-02-16 18:14:59 +0100

    Add todo-comments.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,29 +1,41 @@
 return {
-	"numToStr/Comment.nvim",
-	config = function()
-		require("Comment").setup({
-			padding = true,
-			sticky = true,
-			ignore = nil,
-			toggler = {
-				line = 'gcc',
-				block = 'gbc',
-			},
-			opleader = {
-				line = 'gc',
-				block = 'gb',
-			},
-			extra = {
-				above = 'gcO',
-				below = 'gco',
-				eol = 'gcA',
-			},
-			mappings = {
-				basic = true,
-				extra = true,
-			},
-			pre_hook = nil,
-			post_hook = nil,
-		})
-	end
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup({
+				padding = true,
+				sticky = true,
+				ignore = nil,
+				toggler = {
+					line = 'gcc',
+					block = 'gbc',
+				},
+				opleader = {
+					line = 'gc',
+					block = 'gb',
+				},
+				extra = {
+					above = 'gcO',
+					below = 'gco',
+					eol = 'gcA',
+				},
+				mappings = {
+					basic = true,
+					extra = true,
+				},
+				pre_hook = nil,
+				post_hook = nil,
+			})
+		end
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+
+	}
 }
