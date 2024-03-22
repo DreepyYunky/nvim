2c18a8017ca51f46c26f0ff7fa503a8fd416b1e9
Yunky <nwokeechika@gmail.com>
2024-03-22 19:57:40 +0100

    Update

--- lua/plugins/indentation.lua
+++ lua/plugins/indentation.lua
@@ -1,7 +1,14 @@
 return {
+	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "VimEnter",
 		config = function()
 			require("ibl").setup()
 		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		event = "LazyFile",
+		opts = {}
+	}
 }
