c5653a0fa2c1680b407c931a306422cd8e0aa829
Yunky <nwokeechika@gmail.com>
2024-07-28 16:06:14 +0100

    Fixing indent

--- ftplugin/odin.lua
+++ ftplugin/odin.lua
@@ -1,5 +1,14 @@
 -- ~/.config/nvim/after/ftplugin/odin.lua
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-vim.bo.expandtab = true
+vim.bo.indentexpr = 'v:lua.my_language_indent()'
+
+function _G.my_language_indent()
+    -- Your custom indent logic
+    -- Example: return current indent + 4 spaces
+    local line = vim.fn.getline(vim.v.lnum)
+    if line:match('{') then
+        return vim.fn.indent(vim.v.lnum - 1) + 4
+    else
+        return vim.fn.indent(vim.v.lnum - 1)
+    end
+end
 
